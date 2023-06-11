cdb624d5f479bb9a3f33ce28ce55969c5cbaa620
Zeug555 <sergentzeug@gmail.com>
2023-06-11 17:35:09 +0200

    fix

--- src/files.c
+++ src/files.c
@@ -15,14 +15,14 @@ void readTxt(Product_list *head, char* txt_name)
     {
         /* fscanf renvoie le nombre d’items effectivement lus */
         for (int i=0; i<NB_ELEMENTS; i++)
-        fscanf(fichier , "%s %d %s", head->data.title, &(head->data.price), head->data.desc);
+        fscanf(fichier , "%s %d %s", head->data->title, &(head->data->price), head->data->desc);
         fclose(fichier ); // fermeture du fichier
     }
 }
 
 void printData(Product_list *head)
 {
-    printf("%s %d %s\n", head->data.title, head->data.price, head->data.desc);
+    printf("%s %d %s\n", head->data->title, head->data->price, head->data->desc);
 }
 
 void addNode(Product_list *head)
